971cd85d9f26f8824e31de71a5bfd000f37250f5
danielm2048 <danielmimoun98@gmail.com>
2020-04-23 03:32:07 +0300

    make it better

---
+++ backend/public/precache-manifest.425d6b606caf23b66b3a9776b6c477cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62db3a58188b40618af253f692780f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e48b915a7ba23a5ea884",
+    "url": "/static/css/main.c7306d16.chunk.css"
+  },
+  {
+    "revision": "9f3eb3156f024c1cb469",
+    "url": "/static/js/2.b2c92b84.chunk.js"
+  },
+  {
+    "revision": "6faccae9b2f50564a75f0dc59c0b12b0",
+    "url": "/static/js/2.b2c92b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48b915a7ba23a5ea884",
+    "url": "/static/js/main.5ca368c3.chunk.js"
+  },
+  {
+    "revision": "3e3275f8be82e9b44ddc",
+    "url": "/static/js/runtime-main.f13126c0.js"
+  },
+  {
+    "revision": "b9d3e384704626d621d86495f016dbce",
+    "url": "/static/media/Logo.b9d3e384.png"
+  }
+]);
