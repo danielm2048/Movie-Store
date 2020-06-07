6a7c705bff826337ed821568ed6923d95e2b38f2
danielm2048 <danielmimoun98@gmail.com>
2020-06-08 02:05:13 +0300

    make it better

---
+++ public/precache-manifest.2ed4e717b34a763b66b442644dfa0303.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4ff7bb6f431282cd2db7975664b329",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a71734ebc1b33c99436c",
+    "url": "/static/css/main.c7306d16.chunk.css"
+  },
+  {
+    "revision": "ac8af303063ecf5a8266",
+    "url": "/static/js/2.74a156f8.chunk.js"
+  },
+  {
+    "revision": "600cad0b00fc612b144724c25253f177",
+    "url": "/static/js/2.74a156f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71734ebc1b33c99436c",
+    "url": "/static/js/main.9c82b286.chunk.js"
+  },
+  {
+    "revision": "3e3275f8be82e9b44ddc",
+    "url": "/static/js/runtime-main.f13126c0.js"
+  },
+  {
+    "revision": "ddac56c48117211891e577a56bc45438",
+    "url": "/static/media/checkout.ddac56c4.jpg"
+  },
+  {
+    "revision": "2dfbc131d32cd0824d61063496bf000b",
+    "url": "/static/media/fam.2dfbc131.png"
+  },
+  {
+    "revision": "53cb547ca909828e0a5d0530d0ff954b",
+    "url": "/static/media/friends.53cb547c.png"
+  },
+  {
+    "revision": "524b1e1d49cf65880147cbbe6daa68c2",
+    "url": "/static/media/home.524b1e1d.jpg"
+  },
+  {
+    "revision": "58b7648ecb54b96c3ddd175277014cd0",
+    "url": "/static/media/homeH-min.58b7648e.jpg"
+  },
+  {
+    "revision": "470f3d66fa4ca7c47341dcf63b02a0c1",
+    "url": "/static/media/newLogo.470f3d66.png"
+  },
+  {
+    "revision": "3719f66b2aeb8206476d55a0b58d18a7",
+    "url": "/static/media/red-popcorn-2.3719f66b.png"
+  },
+  {
+    "revision": "1957fb888e541dc1f060b937a028a228",
+    "url": "/static/media/rickmorty.1957fb88.jpg"
+  }
+]);
